d3b0fd8461ad75411d9b6abe9073e5c5280ad579
Jozsef Nagy <nagyjozsef2001@gmail.com>
2023-08-23 16:16:58 +0300

    added searchbar, and the corresponding conditions to the contacts card container

--- frontend/PhoneBook/src/app/pages/searchbar/searchbar.component.ts
+++ frontend/PhoneBook/src/app/pages/searchbar/searchbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-searchbar',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./searchbar.component.css']
 })
 export class SearchbarComponent {
+  enteredText:string=''; //containcs the text entered in the input field
 
+  @Output()
+  searchTextChanged: EventEmitter<string>= new EventEmitter<string>();
+
+  @Output() 
+  searchSelected: EventEmitter<string> = new EventEmitter<string>();
+
+  onSearchTextChanged(){ //emitting a signal with the input text
+    this.searchTextChanged.emit(this.enteredText);
+  }
+
+  onSearchChange(radioInput: string){ //emiting the signal for the corresponding type of search
+    this.searchSelected.emit(radioInput);
+  }
 }
+

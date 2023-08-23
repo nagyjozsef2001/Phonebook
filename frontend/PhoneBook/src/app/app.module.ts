9acbc29e6b111f959d1071dde518b680cd919782
Jozsef Nagy <nagyjozsef2001@gmail.com>
2023-08-23 15:34:39 +0300

    Added navbar, searchbar, home, form and contacts as components and crud as service

--- frontend/PhoneBook/src/app/app.module.ts
+++ frontend/PhoneBook/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchbarComponent } from './pages/searchbar/searchbar.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { FormComponent } from './pages/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    HomeComponent,
+    SearchbarComponent,
+    NavbarComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,

b8e0e1cd2e23f8120c9dbd96e407a7816acb2057
Jozsef Nagy <nagyjozsef2001@gmail.com>
2023-08-25 13:52:35 +0300

    frontend corrected, address forms added

--- frontend/PhoneBook/src/app/pages/form/form.component.ts
+++ frontend/PhoneBook/src/app/pages/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { CrudService } from 'src/app/services/crud.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -16,12 +16,23 @@ export class FormComponent implements OnInit{
     firstName: new FormControl(''),
     lastName: new FormControl(''),
     email: new FormControl(''),
-    notes: new FormControl('')
+    notes: new FormControl(''),
+    country: new FormControl(''),
+    postalCode: new FormControl(''),
+    shire: new FormControl(''),
+    city: new FormControl(''),
+    street: new FormControl(''),
+    number: new FormControl(''),
+    stairs: new FormControl(''),
+    floor: new FormControl(''),
+    apartment: new FormControl('')
   });
 
   task:any="";
 
   createContact:any = {};
+
+  addressContact:any = {};
 
   constructor(private crudService: CrudService, private route: ActivatedRoute){
   }
@@ -49,6 +60,19 @@ export class FormComponent implements OnInit{
       email: this.applyForm.value.email ?? '',
       notes: this.applyForm.value.notes ?? ''
     }
+    this.addressContact = {
+      country: this.applyForm.value.country ?? '',
+      postalCode: this.applyForm.value.postalCode ?? '',
+      shire: this.applyForm.value.shire ?? '',
+      city: this.applyForm.value.city ?? '',
+      street: this.applyForm.value.street ?? '',
+      number: this.applyForm.value.number ?? '',
+      stairs: this.applyForm.value.stairs ?? '',
+      floor: this.applyForm.value.floor ?? '',
+      apartment: this.applyForm.value.apartment ?? ''
+    }
+    this.createContact.address = this.addressContact;
+    console.log(this.createContact);
     this.crudService.modifyContact(this.task,this.createContact).subscribe((result: any) => {
       console.log(result);
   });;
@@ -62,6 +86,19 @@ export class FormComponent implements OnInit{
       email: this.applyForm.value.email ?? '',
       notes: this.applyForm.value.notes ?? ''
     }
+    this.addressContact = {
+      country: this.applyForm.value.country ?? '',
+      postalCode: this.applyForm.value.postalCode ?? '',
+      shire: this.applyForm.value.shire ?? '',
+      city: this.applyForm.value.city ?? '',
+      street: this.applyForm.value.street ?? '',
+      number: this.applyForm.value.number ?? '',
+      stairs: this.applyForm.value.stairs ?? '',
+      floor: this.applyForm.value.floor ?? '',
+      apartment: this.applyForm.value.apartment ?? ''
+    }
+    console.log(this.createContact);
+    this.createContact.address = this.addressContact;
     this.crudService.submitCreate(this.createContact).subscribe((result: any) => {
       console.log(result);
     });;
